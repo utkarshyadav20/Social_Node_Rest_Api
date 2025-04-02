c6d9e903a7c9366c0472f14f897a5d4600eec1dd
RunawayGOD <20022002utkarsh@gmail.com>
2025-04-02 21:42:24 +0530

    updated cross origin policy

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const app = express();
 const cors = require("cors");
 const allowedOrigins = [
   "http://localhost:3000", // Local dev
-  "https://social-app-lilac-five.vercel.app", // Deployed frontend
   "https://master.d1c7vn6uncbq8z.amplifyapp.com",
 ];
 
